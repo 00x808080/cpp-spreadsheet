ced80a81ecf2db7338e1eacbc9be93a996bfc89f
Sergey Gonchar <LornCat@yandex.ru>
2022-12-03 23:08:08 +0100

    initial commit

--- design/cell.h
+++ design/cell.h
@@ -2,22 +2,56 @@
 
 #include "common.h"
 #include "formula.h"
+#incude <optional>
 
-class Cell : public CellInterface {
+class Impl {
 public:
-    Cell();
-    ~Cell();
+    virtual CellInterface::Value GetValue() const = 0;
+    virtual std::string GetText() const = 0;
+};
 
-    void Set(std::string text);
-    void Clear();
+class EmptyImpl: public Impl {
+public:
+    CellInterface::Value GetValue() const override;
+    std::string GetText() const override;
+};
 
-    Value GetValue() const override;
+class TextImpl : public Impl {
+public:
+    TextImpl(std::string text);
+    CellInterface::Value GetValue() const override;
+    std::string GetText() const override;
+private:
+    std::string text_;
+};
+
+class FormulaImpl : public Impl {
+public:
+    FormulaImpl(std::string expression);
+    CellInterface::Value GetValue() const override;
     std::string GetText() const override;
 
 private:
-    class Impl;
-    class EmptyImpl;
-    class TextImpl;
-    class FormulaImpl;
-    std::unique_ptr<Impl> impl_;
+    std::unique_ptr<FormulaInterface> formula_;
+
+    template <typename T>
+    [[nodiscard]] bool Is(const FormulaInterface::Value& value) const {
+        return std::holds_alternative<T>(value);
+    }
+};
+
+class Cell : public CellInterface {
+public:
+    Cell(std::unique_ptr<Impl> impl_);
+    ~Cell();
+
+    Value GetValue() const override;
+    std::string GetText() const override;
+	std::vector<Position> GetReferencedCells();
+	void InvalidateCache();
+	
+private:
+    std::unique_ptr<Impl> impl_ = nullptr;
+	std::vector<Position> list_depended_;
+	std::optional<CellInterface::Value> cache_;
 };
--- design/sheet.h
+++ design/sheet.h
@@ -4,9 +4,16 @@
 #include "common.h"
 
 #include <functional>
+#include <list>
+#include <unordered_map>
 
 class Sheet : public SheetInterface {
 public:
+    using UniqCellPtr = std::unique_ptr<CellInterface>;
+    using ColIterator = std::list<std::list<UniqCellPtr>>::iterator;
+    using RowIterator = std::list<UniqCellPtr>::iterator;
+
+    Sheet();
     ~Sheet();
 
     void SetCell(Position pos, std::string text) override;
@@ -21,14 +28,25 @@ public:
     void PrintValues(std::ostream& output) const override;
     void PrintTexts(std::ostream& output) const override;
 
-    const Cell* GetConcreteCell(Position pos) const;
-    Cell* GetConcreteCell(Position pos);
-
+    
 private:
-    void MaybeIncreaseSizeToIncludePosition(Position pos);
-    void PrintCells(std::ostream& output,
-                    const std::function<void(const CellInterface&)>& printCell) const;
-    Size GetActualSize() const;
+    std::unordered_map<std::string, ColIterator> access_to_col_;
+    std::unordered_map<std::string, RowIterator> access_to_row_;
+    std::list<std::list<UniqCellPtr>> sheet_;
+    Size min_print_area_ = { 0, 0 };
+	
+    void AddCol(Position pos);
+    void AddRow(Position pos);
 
-    std::vector<std::vector<std::unique_ptr<Cell>>> cells_;
+    void ChangePrintAreaByCol();
+    void ChangePrintAreaByRow();
+	
+    std::unique_ptr<Impl> impl_ GetTypeCell(std::string text);
+    void CheckCyclicDependencies();
 };
+
+template <typename T>
+[[nodiscard]] bool Is(const CellInterface::Value& value) {
+    return std::holds_alternative<T>(value);
+}
+
